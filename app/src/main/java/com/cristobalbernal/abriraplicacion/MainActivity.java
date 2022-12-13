d95e5bb3d82059860493b09e3922b08b1e157bb2
Cristobal Bernal Mayordomo <90463533+Racriberny@users.noreply.github.com>
2022-12-13 19:58:38 +0100

    Cosas

--- app/src/main/java/com/cristobalbernal/abriraplicacion/MainActivity.java
+++ app/src/main/java/com/cristobalbernal/abriraplicacion/MainActivity.java
@@ -4,16 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.ActivityNotFoundException;
-import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
@@ -28,11 +24,20 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                abrirAplicaciob("com.cristobalbernal.coches","MainActivity","622047350");
+                abrirAplicaciob("637336566");
             }
         });
     }
-    private void abrirAplicaciob(String paquete, String clase, String tel) {
+    private void abrirAplicaciob(String tel) {
+        int permision = ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
+        if (permision != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL_PHONE);
+        } else {
+            Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + tel));
+            startActivity(callIntent);
+        }
+
+
         /*
         //Abrir la galaria
         Intent intent = new Intent().setAction(Intent.ACTION_VIEW); //Define Intent.
@@ -63,13 +68,6 @@ public class MainActivity extends AppCompatActivity {
         startActivity(abrirAplicacion);
 
          */
-        int permision = ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
-        if (permision != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL_PHONE);
-        } else {
-            Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + tel));
-            startActivity(callIntent);
-        }
 
     }
 }
